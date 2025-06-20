849cf4787e2eca1581d965be8a183a6b5c137139
Marcos Aguare <marcosy300@gmail.com>
2025-06-20 00:32:30 -0600

    Format to files
    Use prettier

--- backend/src/config/dbMongo.js
+++ backend/src/config/dbMongo.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+/** @format */
+
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Conectado a MongoDB Atlas');
-  } catch (err) {
-    console.error('Error al conectar con MongoDB:', err.message);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("Conectado a MongoDB Atlas");
+	} catch (err) {
+		console.error("Error al conectar con MongoDB:", err.message);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
